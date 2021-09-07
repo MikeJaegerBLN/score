3a7b3d2cf0416355fbe6596e9c95e6a5b764f97e
Henry Sallandt <henry.sallandt@arup.com>
2021-09-07 10:58:05 +0200

    Upload Henry

--- XlsxToDict.py
+++ XlsxToDict.py
@@ -1,42 +1,90 @@
 import pandas as pd
+import numpy as np
 import math
 import json
+import pdb
 
 def read_model_inputs(model_file_path, model_inputs_sheet):
 
-    #dictionary to hold model input values
-    model_inputs = {}
 
-    model_inputs_df = pd.read_excel(model_file_path, engine='openpyxl', sheet_name=model_inputs_sheet)
 
-    model_inputs_df = model_inputs_df.iloc[:, 0:4]
-
-    model_inputs_df = model_inputs_df[model_inputs_df['No.'].notna()]
-
-    i = 0
+    model_inputs_dfs = pd.read_excel(model_file_path, engine='openpyxl', sheet_name=model_inputs_sheet)
+    # pdb.set_trace()
     
-    while i < 45:
+    n_AHUs = model_inputs_dfs.shape[1] - 6
+    
+    models_inputs = {}
+    
+    for iAHU in range(n_AHUs):
+        #dictionary to hold model input values
+        model_inputs = {}
+        # pdb.set_trace()
+        model_inputs_df = pd.concat([model_inputs_dfs.iloc[:, 0:3], model_inputs_dfs.iloc[:, 6+iAHU]], axis=1)
+        model_inputs_df.rename(columns={iAHU: 'Value'}, inplace=True)
+        model_inputs_df = model_inputs_df[model_inputs_df['No.'].notna()]
+        # pdb.set_trace()
+        i = 0
         
-        type_param =str(model_inputs_df.loc[i, 'Type'])
-        model_input_param = model_inputs_df.loc[i, 'Parameter']
-        value = model_inputs_df.loc[i, 'Value']
-        if not 'Setpoint' in type_param:
-            model_inputs[model_input_param] = value
-            i=i+1
+        while i < 48:
+            # if i == 42:
+            #     pdb.set_trace()
+            type_param =str(model_inputs_df.loc[i, 'Type'])
+            model_input_param = model_inputs_df.loc[i, 'Parameter']
+            value = model_inputs_df.loc[i, 'Value']
+            if not 'Setpoint' in type_param:
+                model_inputs[model_input_param] = value
+                i=i+1
+            
+            #if type_param value is integer, read corresponding setpoint profiles   
+            else:
+                value_dict            = {}
+                for k in range(i,i+6):
+                    key   = model_inputs_df.loc[k, 'Parameter']
+                    value = model_inputs_df.loc[k, 'Value']
+                    value_dict[key] = value
+                    i += 1
+                # pdb.set_trace()
+                modified_key = int(type_param.split(' ')[0])
+                model_inputs[modified_key] = value_dict
         
-        #if type_param value is integer, read corresponding setpoint profiles   
-        else:
-            value_dict            = {}
-            for k in range(i,i+6):
-                key   = model_inputs_df.loc[k, 'Parameter']
-                value = model_inputs_df.loc[k, 'Value']
-                value_dict[key] = value
-                i += 1
+        models_inputs[iAHU] = model_inputs
+                
+    return models_inputs
+# def read_model_inputs(model_file_path, model_inputs_sheet):
+
+#     #dictionary to hold model input values
+#     model_inputs = {}
+
+#     model_inputs_df = pd.read_excel(model_file_path, engine='openpyxl', sheet_name=model_inputs_sheet)
+
+#     model_inputs_df = model_inputs_df.iloc[:, 0:4]
+
+#     model_inputs_df = model_inputs_df[model_inputs_df['No.'].notna()]
+
+#     i = 0
+    
+#     while i < 45:
+        
+#         type_param =str(model_inputs_df.loc[i, 'Type'])
+#         model_input_param = model_inputs_df.loc[i, 'Parameter']
+#         value = model_inputs_df.loc[i, 'Value']
+#         if not 'Setpoint' in type_param:
+#             model_inputs[model_input_param] = value
+#             i=i+1
+        
+#         #if type_param value is integer, read corresponding setpoint profiles   
+#         else:
+#             value_dict            = {}
+#             for k in range(i,i+6):
+#                 key   = model_inputs_df.loc[k, 'Parameter']
+#                 value = model_inputs_df.loc[k, 'Value']
+#                 value_dict[key] = value
+#                 i += 1
+#             # pdb.set_trace()
+#             modified_key = int(type_param.split(' ')[2])
+#             model_inputs[modified_key] = value_dict
             
-            modified_key = int(type_param.split(' ')[2])
-            model_inputs[modified_key] = value_dict
-            
-    return model_inputs
+#     return model_inputs
 
 
 def read_weather_data(model_file_path, weather_data_sheet):
@@ -50,101 +98,96 @@ def read_weather_data(model_file_path, weather_data_sheet):
     return weather_data_dict
 
 
-def read_run_schedule_profile(model_file_path, run_schedule_sheet):
-
-    run_schedule_df = pd.read_excel(model_file_path, engine='openpyxl', sheet_name=run_schedule_sheet, skiprows=2)
-    #rename first column to date
-    run_schedule_df.rename(columns={'Unnamed: 0' : 'Date'}, inplace=True)
-    #retain 24 hour columns
-    run_schedule_df = run_schedule_df.iloc[:, 0:25]
-    
-    run_schedule_dict = {}
-    run_schedule_dict['Hour'] = []
-    run_schedule_dict['Run_Schedule_Profile'] = []
-    
-    for i in range(8760):
-        day                     = int(i/24)
-        profile_hour            = i - day*24 + 1
-        run_profile_number = run_schedule_df.iloc[day,profile_hour]
-        run_schedule_dict['Hour'].append(int(i+1))
-        run_schedule_dict['Run_Schedule_Profile'].append(int(run_profile_number))
+def read_run_schedule_profile(model_inputs_dict):
+    with open('run_schedules.json', 'r') as f:
+        schedules = json.load(f)
+    n_AHUs = len(model_inputs_dict.keys())
+    run_schedules_dicts = {}
+    for iAHU in range(n_AHUs):
+        run_schedules_dict = {}
+        run_schedules_dict['Hour'] = list(range(1,8761))
         
-    return run_schedule_dict
+        run_schedules_dict['Run_Schedule_Profile'] = schedules[model_inputs_dict[iAHU]['Hours of operation / Part load operation']]['schedule']
+        run_schedules_dicts[iAHU] = run_schedules_dict
+    return run_schedules_dicts
 
 
-def read_thermal_profile(model_file_path, thermal_profile_sheet):
+def read_thermal_profile(models_inputs_dicts, run_schedules_dicts, flag):
+   
+    if flag == 'sensible':
+        dict_key = 'Sensible gains (Heat)'
+    elif flag == 'latent':
+        dict_key = 'Latent gains (Heat)'
+    else:
+        print('Wrong flag!')
+        pdb.set_trace()
+    n_AHUs = len(run_schedules_dicts.keys())
+    thermal_profile_dicts = {}
+    for iAHU in range(n_AHUs):
+        gains_specific = float(models_inputs_dicts[iAHU]['Sensible gains (Heat)'].split(' ')[2][1:]) #W/m²
+        area = float(models_inputs_dicts[iAHU]['Room Area']) # m²
+        gains = gains_specific * area / 1000 #kW
+        
+        thermal_profile = gains * np.array(run_schedules_dicts[iAHU]['Run_Schedule_Profile'])
+        
+        thermal_profile_dict = {}
+        thermal_profile_dict['Hour'] = list(range(1,8761))
+        thermal_profile_dict['Thermal_Profile'] = thermal_profile.tolist()
+        
+        thermal_profile_dicts[iAHU] = thermal_profile_dict
 
-    thermal_profile_df = pd.read_excel(model_file_path, engine='openpyxl', sheet_name=thermal_profile_sheet, skiprows=2)
-    #rename first column to date
-    thermal_profile_df.rename(columns={'Unnamed: 0' : 'Date'}, inplace=True)
-    #retain 24 hour columns
-    thermal_profile_df = thermal_profile_df.iloc[:, 0:25]
-    
-    thermal_profile_dict = {}
-    thermal_profile_dict['Hour'] = []
-    thermal_profile_dict['Thermal_Profile'] = []
-    
-    for i in range(8760):
-        day                     = int(i/24)
-        profile_hour            = i - day*24 + 1
-        thermal_profile         = thermal_profile_df.iloc[day,profile_hour]
-        thermal_profile_dict['Hour'].append(int(i+1))
-        thermal_profile_dict['Thermal_Profile'].append(float(thermal_profile))
-    
-    return thermal_profile_dict
+    return thermal_profile_dicts
 
 
-def read_setpoint_profile(model_file_path, setpoint_profile_sheet):
-
-    setpoint_profile_df = pd.read_excel(model_file_path, engine='openpyxl', sheet_name=setpoint_profile_sheet, skiprows=2)
-    #rename first column to date
-    setpoint_profile_df.rename(columns={'Unnamed: 0' : 'Date'}, inplace=True)
-    #retain 24 hour columns
-    setpoint_profile_df = setpoint_profile_df.iloc[:, 0:25]
-    
-    setpoint_profile_dict = {}
-    setpoint_profile_dict['Hour'] = []
-    setpoint_profile_dict['Setpoint_Profile'] = []
-    
-    for i in range(8760):
-        day                     = int(i/24)
-        profile_hour            = i - day*24 + 1
-        setpoint_profile_number = setpoint_profile_df.iloc[day,profile_hour]
-        setpoint_profile_dict['Hour'].append(int(i+1))
-        setpoint_profile_dict['Setpoint_Profile'].append(int(setpoint_profile_number))
+def read_setpoint_profile(model_inputs_dicts):
+    with open('setpoint_profile.json', 'r') as f:
+        setpoint_profile = json.load(f)
+    n_AHUs = len(model_inputs_dicts.keys())
+    setpoint_profile_dicts = {}
+    for iAHU in range(n_AHUs):
+        setpoint_profile_dict = {}
+        setpoint_profile_dict['Hour'] = list(range(1,8761))
+        setpoint_profile_dict['Setpoint_Profile'] = setpoint_profile
+        setpoint_profile_dicts[iAHU] = setpoint_profile_dict
     return setpoint_profile_dict
 
 
 def read_inputs(model_file_path, model_inputs_sheet, weather_data_sheet, run_schedule_sheet, thermal_profile_sensible_sheet, thermal_profile_latent_sheet,  setpoint_profile_sheet):
 
     #read model inputs
-    model_inputs_dict = read_model_inputs(model_file_path, model_inputs_sheet)
-
+    models_inputs_dicts = read_model_inputs(model_file_path, model_inputs_sheet)
+    
     #read weather data
     weather_data_dict = read_weather_data(model_file_path, weather_data_sheet)
 
     #read run schedule
-    run_schedule_dict = read_run_schedule_profile(model_file_path, run_schedule_sheet)
+    run_schedule_dicts = read_run_schedule_profile(models_inputs_dicts)
 
     #read thermal profile
-    thermal_profile_sensible_dict = read_thermal_profile(model_file_path, thermal_profile_sensible_sheet)
+    thermal_profile_sensible_dicts = read_thermal_profile(models_inputs_dicts, run_schedule_dicts, 'sensible')
 
     #read thermal profile
-    thermal_profile_latent_dict = read_thermal_profile(model_file_path, thermal_profile_latent_sheet)
-
+    thermal_profile_latent_dicts = read_thermal_profile(models_inputs_dicts, run_schedule_dicts, 'latent')
+    
     #read setpoint profile
-    setpoint_profile_dict = read_setpoint_profile(model_file_path, setpoint_profile_sheet)
+    setpoint_profile_dict = read_setpoint_profile(models_inputs_dicts)
 
-    #construct a dictionary
-    inputs_dict = {
-        'model_inputs': model_inputs_dict,
-        'weather_data': weather_data_dict,
-        'run_schedule': run_schedule_dict,
-        'thermal_profile_sensible': thermal_profile_sensible_dict,
-        'thermal_profile_latent': thermal_profile_latent_dict,
-        'setpoint_profile': setpoint_profile_dict
-    }
-
+    #construct a dictionary of dictionaries
+    n_AHUs = len(run_schedule_dicts.keys())
+    inputs_dicts = {}
+    for iAHU in range(n_AHUs):
+            
+        inputs_dict = {
+            'model_inputs': models_inputs_dicts[iAHU],
+            'weather_data': weather_data_dict,
+            'run_schedule': run_schedule_dicts[iAHU],
+            'thermal_profile_sensible': thermal_profile_sensible_dicts[iAHU],
+            'thermal_profile_latent': thermal_profile_latent_dicts[iAHU],
+            'setpoint_profile': setpoint_profile_dict
+        }
+        
+        inputs_dicts[iAHU] = inputs_dict
+    pdb.set_trace()
     return inputs_dict
 
 def tweak_inputs_dict(inputs_dict):
@@ -186,7 +229,7 @@ if __name__ == "__main__":
     with open('run_schedules.json', 'r') as f:
         schedules = json.load(f)
     
-    model_file_path = r'Template.xlsx'
+    model_file_path = r'Template_Batch.xlsx'
     
     model_inputs_sheet = 'Model Inputs'
     weather_data_sheet = 'Weather Data'
@@ -203,8 +246,8 @@ if __name__ == "__main__":
             
     #inputs_dict['run_schedule']['Run_Schedule_Profile'] = schedules[key_to_read]
     
-    #for key in inputs_dict['model_inputs']['General']:
-    #    print (key)
+    for key in inputs_dict['model_inputs']['General']:
+        print (key)
     with open('inputs.json', 'w') as f:
         json.dump(inputs_dict, f)
     
